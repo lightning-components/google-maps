e9a2326403262948dfb080f45611cf1bb737b4e4
Justin Fortunato <jfortunato25@gmail.com>
2020-01-06 12:04:30 -0500

    get an implementation going that works with other browsers

--- lightning-google-maps.js
+++ lightning-google-maps.js
@@ -32,16 +32,96 @@
             this.shadowRoot.appendChild(this.template());
         }
 
+        connectedCallback() {
+            // https://developers.google.com/web/fundamentals/web-components/best-practices#lazy-properties
+            this.upgradeProperty('src');
+
+            // store a reference to the actual iframe that will eventually be loaded
+            this.iframe = this.getIframeElementToBeInserted();
+
+            // if the browser supports native lazy loading, then immediately insert the iframe.
+            // otherwise, setup an observer that will load the iframe when the page is scrolled to it
+            this.supportsNativeLazyLoading() ?
+                this.insertIframe() :
+                this.setupObserver();
+        }
+
         template() {
             const cloned = template.content.cloneNode(true);
 
-            let iframe = replaceTag(this);
-            iframe = iframe.replace(/ src=/g, ' loading="lazy" src=');
-
-            const container = cloned.querySelector('.lightning-google-maps');
-            container.innerHTML = iframe;
-
             return cloned;
+        }
+
+        getIframeElementToBeInserted() {
+            // get the current lightning-google-maps element and replace
+            // it with iframe, and we should have a valid google maps embed iframe
+            const template = document.createElement('template');
+            template.innerHTML = replaceTag(this);
+            const iframe = template.content.firstChild;
+
+            if (this.supportsNativeLazyLoading()) {
+                // for browsers that support native lazy loading, return the true iframe tag
+                // with the real src but ensure it has the loading=lazy attribute
+                iframe.loading = 'lazy';
+            }
+
+            // add our own class to the iframe
+            iframe.classList.add('lightning-google-maps__iframe')
+
+            return iframe;
+        }
+
+        supportsNativeLazyLoading() {
+            return 'loading' in HTMLImageElement.prototype;
+        }
+
+        setupObserver() {
+            const observer = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        this.insertIframe();
+
+                        observer.unobserve(this);
+                    }
+                })
+            }, { rootMargin: '0px' });
+
+            observer.observe(this);
+        }
+
+
+        insertIframe() {
+            const container = this.shadowRoot.querySelector('.lightning-google-maps');
+            // clear all contents
+            container.innerHTML = '';
+            // insert the iframe into the shadow DOM
+            container.appendChild(this.iframe);
+        }
+
+        upgradeProperty(prop) {
+            if (this.hasOwnProperty(prop)) {
+                let value = this[prop];
+                delete this[prop];
+                this[prop] = value;
+            }
+        }
+
+        /**
+         * Ensure src property is reflected to an attribute.
+         * https://developers.google.com/web/fundamentals/web-components/customelements#properties_and_attributes
+         */
+        set src(value) {
+            if (!value) {
+                this.removeAttribute('src');
+
+                return;
+            }
+
+            this.setAttribute('src', value);
+        }
+
+        get src() {
+            return this.getAttribute('src');
         }
     }
 
@@ -65,82 +145,3 @@
 
     customElements.define('lightning-google-maps', LightningGoogleMaps);
 })();
-
-
-// class LightningGoogleMaps extends HTMLElement {
-//     static DEFAULT_WIDTH = 600;
-//     static DEFAULT_HEIGHT = 450;
-//
-//     constructor() {
-//         super();
-//
-//         // here, we are going to store the exact iframe supplied to us, so that we can
-//         // use it later when we replace the placeholder image with the actual iframe
-//         const template = document.createElement('template');
-//         template.innerHTML = this.querySelector('noscript').innerHTML.trim();
-//         this.iframe = template.content.firstChild;
-//
-//         this.iframe.onload = () => {
-//             this.hidePlaceholderImage();
-//         };
-//
-//         // we don't need the noscript or iframe anymore
-//         this.innerHTML = '';
-//
-//         this.width = this.iframe.width || LightningGoogleMaps.DEFAULT_WIDTH;
-//         this.height = this.iframe.height || LightningGoogleMaps.DEFAULT_HEIGHT;
-//         this.attachShadow({mode: 'open'}).innerHTML = this.template();;
-//     }
-//
-//     connectedCallback() {
-//         this.addEventListener('mouseover', this.insertIframe);
-//         window.addEventListener('load', () => this.insertIframe());
-//     }
-//
-//     template() {
-//         return `
-//         <style>
-//         .lightning-google-maps {
-//             display: inline-block;
-//             cursor: pointer;
-//         }
-//         .lightning-google-maps__placeholder-image {
-//             position: absolute;
-//             left: 0;
-//             opacity: 1;
-//             transition: opacity 0.4s;
-//         }
-//         .lightning-google-maps__placeholder-image--hidden {
-//             opacity: 0;
-//         }
-//         </style>
-//
-//         <div class="lightning-google-maps">
-//             <img class="lightning-google-maps__placeholder-image" width="${this.width}" height="${this.height}" loading="lazy" src="https://placehold.it/${this.width}x${this.height}" alt="Google Maps Placeholder Image" />
-//         </div>
-//         `;
-//     }
-//
-//     hidePlaceholderImage() {
-//         this.shadowRoot.querySelector('.lightning-google-maps__placeholder-image').classList.add('lightning-google-maps__placeholder-image--hidden')
-//     }
-//
-//     insertIframe() {
-//         const container = this.shadowRoot.querySelector('.lightning-google-maps');
-//
-//         // add our own class to the iframe
-//         this.iframe.classList.add('lightning-google-maps__iframe')
-//
-//         // ensure the iframe is the same dimensions as the placeholder image
-//         this.iframe.width = this.width;
-//         this.iframe.height = this.height;
-//
-//         // insert the iframe before the placeholder image so we can smoothly transition from the image to the iframe
-//         container.prepend(this.iframe);
-//
-//         // we only need to insert the iframe once
-//         this.removeEventListener('mouseover', this.insertIframe);
-//     }
-// }
-//
-// customElements.define('lightning-google-maps', LightningGoogleMaps);
